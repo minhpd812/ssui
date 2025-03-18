4990d3f1d6b7925715cc6e364b7151dc5bd6d28e
Phạm Duy Minh <minh.pd@supertek.vn>
2025-03-18 19:58:11 +0700

    feat: update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    dts({
+      insertTypesEntry: true, // Đảm bảo tạo file index.d.ts
+      tsconfigPath: "tsconfig.node.json",
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
