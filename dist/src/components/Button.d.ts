af28bfd5a3e8a16e09c6f4f44e210fd846a9efce
Phạm Duy Minh <minh.pd@supertek.vn>
2025-03-19 13:49:47 +0700

    chore: remove dist folder

--- dist/src/components/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import { default as React } from 'react';
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: "primary" | "secondary";
-}
-declare const Button: React.FC<ButtonProps>;
-export default Button;
